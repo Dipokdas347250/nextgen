2b294b79a4cb8e293189ab1fa65d3a499eb25bca
Dipokdas347250 <dipakdas24680@gmail.com>
2026-06-16 12:04:38 +0600

    nodemailer chack

--- services/mailService.js
+++ services/mailService.js
@@ -1,12 +1,15 @@
 const nodemailer = require("nodemailer");
 
-const transporter =
-  nodemailer.createTransport({
+const transporter = nodemailer.createTransport({
     service: "gmail",
+     host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
+     family: 4,
   });
 
 const sendMail = async (
